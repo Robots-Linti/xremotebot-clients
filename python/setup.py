8f764334c5d65a2bd1338218d01e11eab2d740e8
Fernando López <soportelihuen@linti.unlp.edu.ar>
2015-04-30 17:08:47 -0300

    setup.py

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='xremotebot-clients',
+      version='1.0',
+      description='XRemoteBot official Python client',
+      author='Fernando López',
+      author_email='flopez@linti.unlp.edu.ar',
+      url='https://github.com/fernandolopez/xremotebot-clients',
+      packages=['xremotebot'],
+      install_requires=[
+            'websocket-client',
+      ],
+     )
